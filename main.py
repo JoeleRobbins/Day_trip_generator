73cfacd44e57f4ca969408dd63aaeba06caef2dc
Joele Robbins <joelerobbins@yahoo.com>
2022-09-07 12:52:26 -0500

    Original Template For Project

---
+++ main.py
@@ -0,0 +1,29 @@
+destinations = ["Canada, Japan, Sweden, Australia"]
+
+restarurants = ["KFC, McDonalds, Pizza Hut, Burger King"]
+
+mode_of_transportation = ["bicycling, personal car, skateboarding, walking"]
+
+entertainments = ["Movies, clubs, amusement parks, sky diving"]
+
+#Random Day Trip Generator
+
+def run_day_trip_generator (): 
+
+
+
+def print_trip(): --
+
+
+def generate_random_item (): -- 
+
+
+def determine_satisfaction (): --
+
+
+def reselect_options (): ---
+
+
+run_day_trip_generator()
+
+
