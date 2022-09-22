4ce30f61d0bfc1df062c654d5bd01797cef44c87
Joele Robbins <joelerobbins@yahoo.com>
2022-09-21 23:42:43 -0500

    Added all the random modules to my variables.

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import random
 
 destinations = ['Canada', 'Japan', 'Sweden', 'Australia']
 
-restarurants = ['KFC', 'McDonalds', 'Pizza Hut', 'Burger King']
+restaurants = ['KFC', 'McDonalds', 'Pizza Hut', 'Burger King']
 
 mode_of_transportations = ['bicycling', 'personal car', 'skateboarding', 'walking']
 
 entertainments = ['Movies', 'clubs', 'amusement parks', 'sky diving']
 
 random_destination = random.choice(destinations)
+
+random_restaurants = random.choice(restaurants)
+
+random_mode_of_transportations = random.choice(mode_of_transportations)
+
+random_entertainments = random.choice(entertainments)
 
 #Random Day Trip Generator
 
